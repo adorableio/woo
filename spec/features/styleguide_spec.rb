325689eb426f246878b1feea8a5ce61b66cdd83e
Jim Remsik <jim@adorable.io>
2014-09-27 19:43:59 -0500

    Remove superfluous context

--- spec/features/styleguide_spec.rb
+++ spec/features/styleguide_spec.rb
@@ -4,10 +4,11 @@ require 'spec_helper'
 
 describe 'Styleguide home' do
 
-  context 'with an issued loan' do
-    it 'is on Account home' do
-      visit '/styleguide'
-      expect(current_path).to eq '/styleguide'
-    end
+  before do
+    visit '/styleguide'
+  end
+
+  it 'is loaded' do
+    expect(current_path).to eq '/styleguide'
   end
 end
