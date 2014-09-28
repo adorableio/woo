2497ff9bad47d78537b8ef166f4c2ce05ef7f73c
Jim Remsik <jim@adorable.io>
2014-09-27 19:12:57 -0500

    Cleanup; Whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,10 @@ require 'factory_girl_rails'
 require 'capybara/rspec'
 
 Rails.backtrace_cleaner.remove_silencers!
+
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = true
