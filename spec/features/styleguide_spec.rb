36767c727447c07d51557b576bdf7bde30da21b7
Jim Remsik <jim@adorable.io>
2014-09-27 20:00:30 -0500

    Add resizing specs

--- spec/features/styleguide_spec.rb
+++ spec/features/styleguide_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'Styleguide home' do
+describe 'Styleguide' do
 
   before do
     visit '/styleguide'
@@ -10,5 +10,37 @@ describe 'Styleguide home' do
 
   it 'is loaded' do
     expect(current_path).to eq '/styleguide'
+  end
+
+  it 'has a width menu' do
+    expect(page).to have_content('Width')
+  end
+
+  it 'resizes to 320px width' do
+    expect(page).to have_content('320')
+  end
+
+  it 'resizes to 480px width' do
+    expect(page).to have_content('480')
+  end
+
+  it 'resizes to 748px width' do
+    expect(page).to have_content('748')
+  end
+
+  it 'resizes to 960px width' do
+    expect(page).to have_content('960')
+  end
+  it 'resizes to 1024px width' do
+    expect(page).to have_content('1024')
+  end
+  it 'resizes to 1280px width' do
+    expect(page).to have_content('1280')
+  end
+  it 'resizes to iPhone resolution' do
+    expect(page).to have_content('iPhone')
+  end
+  it 'resizes to Full Screen width' do
+    expect(page).to have_content('Full')
   end
 end
